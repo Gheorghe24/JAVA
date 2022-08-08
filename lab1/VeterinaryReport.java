ca9895764ec79780c30b356d7dd6b1252d6018ea
Gheorghe24 <gheorghe231101@yahoo.com>
2022-08-08 22:54:47 +0300

    added first file in this repository

---
+++ lab1/VeterinaryReport.java
@@ -0,0 +1,12 @@
+public class VeterinaryReport { 
+     int dogs; 
+     int cats; 
+ 
+     public int getAnimalsCount() { 
+         return dogs + cats;
+     } 
+ 
+     public void displayStatistics() { 
+         System.out.println("Total number of animals is " + getAnimalsCount());
+     } 
+ } 
