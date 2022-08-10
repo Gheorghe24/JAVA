effea3147ab2ccc61da7b9f62b26a6fdb2848176
Gheorghe24 <gheorghe231101@yahoo.com>
2022-08-10 18:36:04 +0300

    lab1

--- demo1/src/VeterinaryTest.java
+++ demo1/src/VeterinaryTest.java
@@ -4,13 +4,12 @@ public class VeterinaryTest {
         vr.setDogs(3);
         vr.setCats(7);
 
-        vr.displayStatistics();
-
         VeterinaryReport vr2 = new VeterinaryReport();
         vr2.setDogs(4);
         vr2.setCats(2);
 
-
         vr2.displayStatistics();
+
+        System.out.println("mi0000000i");
     }
 }
--- src/untitled/src/POO.java
+++
@@ -1,5 +0,0 @@
-public class POO {
-    public static void main(String[] args) {
-        System.out.println("Hello, POO students!!!");
-    }
-}
