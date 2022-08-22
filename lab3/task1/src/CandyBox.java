83c4082df7d7eff525036d7bd2351ec3938fe996
Gheorghe24 <gheorghe231101@yahoo.com>
2022-08-22 23:26:52 +0300

    lab3

---
+++ lab3/task1/src/CandyBox.java
@@ -0,0 +1,50 @@
+import java.lang.Math;
+public class CandyBox {
+    private String flavor, origin;
+
+    public CandyBox(String flavor, String origin) {
+        this.flavor = flavor;
+        this.origin = origin;
+    }
+
+    public CandyBox() {
+
+    }
+
+    float getVolume(){
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "CandyBox{" +
+                "flavor='" + flavor + '\'' +
+                ", origin='" + origin + '\'' +
+                '}';
+    }
+}
+
+class Lindt extends CandyBox {
+    float lenght, width, height;
+@Override
+    float getVolume(){
+        return lenght * width * height;
+    }
+}
+
+class Baravelli extends CandyBox {
+    float radius, height;
+
+    @Override
+    float getVolume(){
+        return radius * height;
+    }
+}
+
+class ChocAmor extends CandyBox {
+    float lenght;
+    @Override
+    float getVolume(){
+        return (float)Math.pow(lenght,3);
+    }
+}
